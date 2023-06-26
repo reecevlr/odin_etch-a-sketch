c3ff95ce0d05809413b04e1ae21168704cb978a5
reece-vlr <reece.vlr@gmail.com>
2023-06-26 21:20:39 +0800

    Fix createGrid function

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-const containerGrid = document.querySelector('.container-main-grid');
+const containerGrid = document.querySelector('.container-grid');
 const gridBtn = document.querySelector('#grid-btn');
 
 function createGrid(gridSize) {
-    const row = document.createElement('div');
     gridSize = 16;
 
     for (let i = 0; i < gridSize; i++) {
-        for (let j = 0; j < gridSize; j++) {
+        for (let j = 0; j <= gridSize; j++) {
             const cell = document.createElement('div');
             cell.classList.add('grid-cell');
-            cell.textContent = 'OMG';
-            row.append(cell);
+            // cell.textContent = (i * gridSize) + j;
+            containerGrid.append(cell);
         }
-        containerGrid.appendChild(row);
     }
+    containerGrid.style = `grid-template-columns: 
+        repeat(${gridSize}, 0fr)`;
 }
 
 // Event Listeners
