e8fd0ff1d33cb55a9ff40ff9ff36019e82a16bfd
reece-vlr <reece.vlr@gmail.com>
2023-06-26 22:06:04 +0800

    Add variables, functions, and event listeners
    initGrid()
    - creates initial 16x16 grid
    
    resetGrid()
    - deletes previous grid
    
    getGridSize()
    - returns input grid size

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 const containerGrid = document.querySelector('.container-grid');
+const inSize = document.querySelector('#input-size');
+const displaySize = document.querySelector('#display-size');
 const gridBtn = document.querySelector('#grid-btn');
 
+function initGrid() {
+    let gridSize = 16;
+
+    for (let i = 0; i < gridSize; i++) {
+        for (let j = 0; j <= gridSize; j++) {
+            const cell = document.createElement('div');
+            cell.classList.add('grid-cell');
+            // cell.textContent = (i * gridSize) + j;
+            containerGrid.append(cell);
+        }
+    }
+}
+
 function createGrid(gridSize) {
-    gridSize = 16;
+    resetGrid();
 
     for (let i = 0; i < gridSize; i++) {
         for (let j = 0; j <= gridSize; j++) {
@@ -16,5 +31,24 @@ function createGrid(gridSize) {
         repeat(${gridSize}, 0fr)`;
 }
 
+function getGridSize() {
+    displaySize.textContent = inSize.value;
+}
+
+function resetGrid() {
+    const cells = document.querySelectorAll('.grid-cell');
+
+    cells.forEach(cell => {
+        cell.parentNode.removeChild(cell);
+    });
+}
+
 // Event Listeners
-gridBtn.addEventListener('click', createGrid);
+gridBtn.addEventListener('click', function(e) {
+    createGrid(inSize.value);
+});
+
+inSize.addEventListener('input', getGridSize);
+
+// Create initial grid of 16x16
+initGrid();
