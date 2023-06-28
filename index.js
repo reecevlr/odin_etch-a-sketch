16812254a2b261d3a851eb51054b6662c6d05f2f
reece-vlr <reece.vlr@gmail.com>
2023-06-28 21:57:07 +0800

    Add functions for pen-trail effect

--- index.js
+++ index.js
@@ -1,8 +1,29 @@
 const containerGrid = document.querySelector('.container-grid');
+// Control Settings
+const clearBtn = document.querySelector('#clr-btn');
+// Size Settings
 const inSize = document.querySelector('#input-size');
 const displaySize = document.querySelector('#display-size');
 const gridBtn = document.querySelector('#grid-btn');
 
+// Create initial grid of 16x16
+createGrid(16);
+
+// Event Listeners
+clearBtn.addEventListener('click', function(e) {
+    getGridCells().forEach(cell => {
+        // Default style
+        cell.style.backgroundColor = 'transparent';
+    });
+});
+
+inSize.addEventListener('input', getGridSize);
+
+gridBtn.addEventListener('click', function(e) {
+    createGrid(inSize.value);
+});
+
+// Functions
 function createGrid(gridSize) {
     resetGrid();
 
@@ -14,14 +35,14 @@ function createGrid(gridSize) {
             containerGrid.append(cell);
         }
     }
+    const gridCells = document.querySelectorAll('.grid-cell');
+    setGridCells(gridCells);
+    setCellListener(gridCells);
+
     containerGrid.style = `grid-template-rows: 
         repeat(${gridSize}, 1fr)`;
     containerGrid.style = `grid-template-columns: 
         repeat(${gridSize}, 1fr)`;
-}
-
-function getGridSize() {
-    displaySize.textContent = inSize.value;
 }
 
 function resetGrid() {
@@ -32,12 +53,37 @@ function resetGrid() {
     });
 }
 
-// Event Listeners
-gridBtn.addEventListener('click', function(e) {
-    createGrid(inSize.value);
-});
+function setCellListener(gridCells) {
+    let flag = false;
 
-inSize.addEventListener('input', getGridSize);
+    window.onmouseup = () => {
+        flag = false;
+    }
 
-// Create initial grid of 16x16
-createGrid(16);
+    gridCells.forEach(cell => {
+        cell.onmouseover = () => {
+            if (flag) {
+                cell.style.backgroundColor = '#e9eaec';
+            }
+        }
+        cell.onmousedown = () => {
+            cell.style.backgroundColor = '#e9eaec';
+            flag = true;
+        }
+    });
+}
+
+function setGridCells(cells) {
+    this.gridCells = cells;
+}
+
+function getGridCells() {
+    if (gridCells === 'undefined') {
+        gridCells = document.querySelectorAll('.grid-cell');
+    }
+    return gridCells;
+}
+
+function getGridSize() {
+    displaySize.textContent = inSize.value;
+} 
